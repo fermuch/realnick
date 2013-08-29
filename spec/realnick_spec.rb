b22a9d56d8e16ae09fafd5a033b2cbfa7ef70cfc
Fernando Mumbach <fermuch@gmail.com>
2013-08-29 11:57:02 -0300

    added tests

--- spec/realnick_spec.rb
+++ spec/realnick_spec.rb
@@ -7,16 +7,30 @@ describe Realnick do
     subject.const_get('VERSION').should_not be_empty
   end
 
-  it "should raise error when method is not valid on fetch" do
+  it "should raise error when method is not valid" do
     expect {
       Realnick.fetch(:method => 'I am not a valid method')
     }.to raise_error(ArgumentError)
   end
 
   it "should return a valid nick" do
-    expect {
-      Realnick.fetch.first
-    }.to be_an_instance_of(String)
+    Realnick.fetch.first.should be_an_instance_of(String)
+  end
+
+  it "should work with method popular_anime" do
+    Realnick.fetch('popular_anime').first.should be_an_instance_of(String)
+  end
+
+  it "should work with method top_anime" do
+    Realnick.fetch('top_anime').first.should be_an_instance_of(String)
+  end
+
+  it "should work with method upcoming_anime" do
+    Realnick.fetch('upcoming_anime').first.should be_an_instance_of(String)
+  end
+
+  it "should work with method just_added_anime" do
+    Realnick.fetch('just_added_anime').first.should be_an_instance_of(String)
   end
 
 end
